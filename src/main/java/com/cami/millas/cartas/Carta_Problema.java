70609bbc5092894ae5588f96805f26792acbb656
c-bernardez <cbernardez@mail.utdt.edu>
2026-03-11 17:04:35 -0300

    added Problema to enum and finished annotations for Carta_Problema

--- src/main/java/com/cami/millas/cartas/Carta_Problema.java
+++ src/main/java/com/cami/millas/cartas/Carta_Problema.java
@@ -1,11 +1,20 @@
 package com.cami.millas.cartas;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Carta_Problema extends Carta {
-    private String tipo_problema; //[STOP, VELOCIDAD MAXIMA, FALTA DE COMBUSTIBLE, CHOQUE, PINCHADURA]
+    @Enumerated(EnumType.STRING)
+    @Column(name="tipo_problema", nullable = false)
+    private TipoProblema tipo_problema;
 
     public Carta_Problema() {}
 
-    public Carta_Problema(int id, String tipo_problema){
+    public Carta_Problema(int id, TipoProblema tipo_problema){
         super(id, "PROBLEMA");
         this.tipo_problema = tipo_problema;
     }
---
+++ src/main/java/com/cami/millas/cartas/TipoProblema.java
@@ -0,0 +1,5 @@
+package com.cami.millas.cartas;
+
+public enum TipoProblema {
+    STOP, velocidadMAX, faltaCOMBUSTIBLE, CHOQUE, PINCHADURA
+}
