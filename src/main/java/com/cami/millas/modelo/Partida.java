eda36cf358b4d2a61d46ed772f9473e733ffb808
c-bernardez <cbernardez@mail.utdt.edu>
2026-03-18 11:49:15 -0300

    had forgotten fecha attribute

--- src/main/java/com/cami/millas/modelo/Partida.java
+++ src/main/java/com/cami/millas/modelo/Partida.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Entity
 @Getter
 @Setter
@@ -17,9 +19,13 @@ public class Partida {
     @Column(nullable = false)
     private EstadoPartida estado;
 
+    @Column(nullable = false)
+    private LocalDate fecha;
+
     public Partida() {}
 
     public Partida(EstadoPartida estado) {
         this.estado = estado;
+        this.fecha = LocalDate.now();
     }
 }
